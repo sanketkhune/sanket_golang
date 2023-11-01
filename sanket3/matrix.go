fa4f7c7054e36fdda82e19a97e05baf0b04fe6a0
sanketkhune <123212791+sanketkhune@users.noreply.github.com>
2023-11-01 12:52:10 +0530

    Update matrix.go

---
+++ sanket3/matrix.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Create a slice of slices to represent the matrix.
+	var matrix [][]int = make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		// Create a new slice for each row of the matrix.
+		matrix[i] = make([]int, 3)
+	}
+	// Populate the matrix with values.
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			matrix[i][j] = i + j
+		}
+	}
+	// Print the matrix.
+	for i := 0; i < 3; i++ {
+		fmt.Println(matrix[i])
+	}
+}
